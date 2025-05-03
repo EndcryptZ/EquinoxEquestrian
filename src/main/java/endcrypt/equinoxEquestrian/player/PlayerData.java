57b1ff045af5fd52c4b02d06657f06a587af16f3
ptern <pternej32@gmail.com>
2025-05-03 22:27:52 +0800

    wip: updates

---
+++ src/main/java/endcrypt/equinoxEquestrian/player/PlayerData.java
@@ -0,0 +1,30 @@
+package endcrypt.equinoxEquestrian.player;
+
+import org.bukkit.entity.AbstractHorse;
+
+public class PlayerData {
+
+    private AbstractHorse selectedHorse;
+    private int tokens;
+
+    public PlayerData(AbstractHorse selectedHorse, int tokens) {
+        this.selectedHorse = selectedHorse;
+        this.tokens = tokens;
+    }
+
+    public AbstractHorse getSelectedHorse() {
+        return selectedHorse;
+    }
+
+    public void setSelectedHorse(AbstractHorse selectedHorse) {
+        this.selectedHorse = selectedHorse;
+    }
+
+    public int getTokens() {
+        return tokens;
+    }
+
+    public void setTokens(int tokens) {
+        this.tokens = tokens;
+    }
+}
