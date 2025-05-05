86d78e1043d33e07ade6e6a2f04c1acb85c392dc
ptern <pternej32@gmail.com>
2025-05-06 00:46:43 +0800

    add: added `info` command

--- src/main/java/endcrypt/equinoxEquestrian/commands/HorseCommand.java
+++ src/main/java/endcrypt/equinoxEquestrian/commands/HorseCommand.java
@@ -1,12 +1,12 @@
 package endcrypt.equinoxEquestrian.commands;
 
 import endcrypt.equinoxEquestrian.EquinoxEquestrian;
+import endcrypt.equinoxEquestrian.commands.horsesubcommands.InfoCommand;
 import endcrypt.equinoxEquestrian.commands.horsesubcommands.ListCommand;
 import endcrypt.equinoxEquestrian.commands.horsesubcommands.TokensCommand;
 import org.bukkit.command.Command;
 import org.bukkit.command.CommandExecutor;
 import org.bukkit.command.CommandSender;
-import org.jetbrains.annotations.NotNull;
 
 public class HorseCommand implements CommandExecutor {
 
@@ -14,11 +14,13 @@ public class HorseCommand implements CommandExecutor {
     private final EquinoxEquestrian plugin;
     private final ListCommand listCommand;
     private final TokensCommand tokensCommand;
+    private final InfoCommand infoCommand;
 
     public HorseCommand(EquinoxEquestrian plugin) {
         this.plugin = plugin;
         this.listCommand = new ListCommand(plugin);
         this.tokensCommand = new TokensCommand(plugin);
+        this.infoCommand = new InfoCommand(plugin);
     }
 
     @Override
@@ -30,6 +32,9 @@ public class HorseCommand implements CommandExecutor {
                     break;
                 case "tokens":
                     tokensCommand.execute(commandSender, command, args);
+                    break;
+                case "info":
+                    infoCommand.execute(commandSender, args);
                     break;
                 default:
                     commandSender.sendMessage("Unknown command.");
