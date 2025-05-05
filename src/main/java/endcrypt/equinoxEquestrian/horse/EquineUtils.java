6caecc86b9cfeca56bcb8b964c8af62a42abd046
ptern <pternej32@gmail.com>
2025-05-06 00:45:59 +0800

    add: added more utility methods

--- src/main/java/endcrypt/equinoxEquestrian/horse/EquineUtils.java
+++ src/main/java/endcrypt/equinoxEquestrian/horse/EquineUtils.java
@@ -1,13 +1,13 @@
 package endcrypt.equinoxEquestrian.horse;
 
 import de.tr7zw.changeme.nbtapi.NBT;
-import org.bukkit.entity.AbstractHorse;
-import org.bukkit.entity.Entity;
-import org.bukkit.entity.LivingEntity;
-import org.bukkit.entity.Player;
+import endcrypt.equinoxEquestrian.horse.enums.*;
+import org.bukkit.entity.*;
 import org.bukkit.inventory.ItemStack;
 
+import java.text.SimpleDateFormat;
 import java.util.ArrayList;
+import java.util.Date;
 import java.util.List;
 
 public class EquineUtils {
@@ -59,4 +59,60 @@ public class EquineUtils {
         String isGroomItem = NBT.get(item, nbt -> (String) nbt.getString("EQUINE_GROOM_ITEM"));
         return isGroomItem.equalsIgnoreCase("true");
     }
+
+    public static String getHorseClaimdate(AbstractHorse horse) {
+        long claimEpoch = NBT.getPersistentData(horse, nbt -> nbt.getLong("EQUINE_CLAIM_TIME"));
+
+        Date date = new Date(claimEpoch);
+
+        // Format date to "DD MMM YYYY"
+        SimpleDateFormat formatter = new SimpleDateFormat("dd MMM yyyy");
+
+        return formatter.format(date);
+    }
+
+    public static EquineHorse fromAbstractHorse(AbstractHorse horse) {
+        return NBT.getPersistentData(horse, nbt -> {
+            String name = horse.getCustomName();
+            int age = nbt.getInteger("EQUINE_AGE");
+            double heightHands = nbt.getDouble("EQUINE_HEIGHT");
+
+            Discipline discipline = Discipline.valueOf(nbt.getString("EQUINE_DISCIPLINE"));
+            Breed breed = Breed.valueOf(nbt.getString("EQUINE_BREED"));
+            Gender gender = Gender.valueOf(nbt.getString("EQUINE_GENDER"));
+
+            // Extract coat color and modifier from Horse
+            CoatColor coatColor = CoatColor.NONE;
+            CoatModifier coatModifier = CoatModifier.NONE;
+
+            if (horse instanceof Horse h) {
+                coatColor = CoatColor.getCoatColorFromHorseColor(h.getColor());
+                coatModifier = CoatModifier.getCoatModifierFromHorseStyle(h.getStyle());
+            }
+
+            // Traits
+            List<Trait> traits = new ArrayList<>(3);
+            for (int i = 0; i < 3; i++) {
+                traits.add(Trait.valueOf(nbt.getString("EQUINE_TRAIT_" + i)));
+            }
+
+            EquineHorse equineHorse = new EquineHorse(
+                    name,
+                    discipline,
+                    breed,
+                    coatColor,
+                    coatModifier,
+                    gender,
+                    age,
+                    Height.getByHands(heightHands),
+                    traits.toArray(new Trait[0])
+            );
+
+
+            return equineHorse;
+        });
+    }
+
+
+
 }
