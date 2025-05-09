eda9010a21a979aef6ec68f4fe17cce16db9fe62
Endcrypt <pternej32@gmail.com>
2025-05-10 03:29:56 +0800

    update: Admin token command to give, take, set tokens of players

---
+++ src/main/java/endcrypt/equinoxEquestrian/commands/equineadmin/subcommands/TokenCommand.java
@@ -0,0 +1,18 @@
+package endcrypt.equinoxEquestrian.commands.equineadmin.subcommands;
+
+import endcrypt.equinoxEquestrian.EquinoxEquestrian;
+import org.bukkit.command.Command;
+import org.bukkit.command.CommandSender;
+
+public class TokenCommand {
+
+    private final EquinoxEquestrian plugin;
+
+    public TokenCommand(EquinoxEquestrian plugin) {
+        this.plugin = plugin;
+    }
+
+    public void execute(CommandSender sender, Command command, String[] args) {
+
+    }
+}
