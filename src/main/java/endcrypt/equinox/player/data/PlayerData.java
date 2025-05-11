fe4755ac1b030d477fe800ceabf55895cd64b467
Endcrypt <pternej32@gmail.com>
2025-05-12 02:41:10 +0800

    update: added UUID List of horses for database efficiency

--- src/main/java/endcrypt/equinox/player/data/PlayerData.java
+++ src/main/java/endcrypt/equinox/player/data/PlayerData.java
@@ -2,29 +2,44 @@ package endcrypt.equinox.player.data;
 
 import org.bukkit.entity.AbstractHorse;
 
+import java.util.List;
+import java.util.UUID;
+
 public class PlayerData {
 
     private AbstractHorse selectedHorse;
     private int tokens;
+    private List<UUID> ownedHorses;
 
-    public PlayerData(AbstractHorse selectedHorse, int tokens) {
+    public PlayerData(AbstractHorse selectedHorse, int tokens, List<UUID> ownedHorses) {
         this.selectedHorse = selectedHorse;
         this.tokens = tokens;
+        this.ownedHorses = ownedHorses;
     }
+
 
     public AbstractHorse getSelectedHorse() {
         return selectedHorse;
+    }
+
+
+    public int getTokens() {
+        return tokens;
+    }
+
+    public List<UUID> getOwnedHorses() {
+        return ownedHorses;
     }
 
     public void setSelectedHorse(AbstractHorse selectedHorse) {
         this.selectedHorse = selectedHorse;
     }
 
-    public int getTokens() {
-        return tokens;
-    }
-
     public void setTokens(int tokens) {
         this.tokens = tokens;
+    }
+
+    public void addOwnedHorse(UUID uuid) {
+        ownedHorses.add(uuid);
     }
 }
