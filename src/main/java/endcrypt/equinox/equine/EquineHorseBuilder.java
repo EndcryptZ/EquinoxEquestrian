6a5e964bf4fe77e62c3882ebd179d4702d47ce40
Endcrypt <pternej32@gmail.com>
2025-05-12 01:01:18 +0800

    update: Skull ID is now assigned on EquineHorse spawn

--- src/main/java/endcrypt/equinox/equine/EquineHorseBuilder.java
+++ src/main/java/endcrypt/equinox/equine/EquineHorseBuilder.java
@@ -12,6 +12,9 @@ import org.bukkit.entity.Horse;
 import org.bukkit.entity.Player;
 
 import java.sql.SQLException;
+import java.util.Arrays;
+import java.util.List;
+import java.util.Random;
 import java.util.stream.IntStream;
 
 public class EquineHorseBuilder {
@@ -58,29 +61,36 @@ public class EquineHorseBuilder {
 
 
             long currentTime = System.currentTimeMillis();
-            nbt.setLong("EQUINE_CLAIM_TIME", currentTime);
-            nbt.setLong("EQUINE_BIRTH_TIME", currentTime - (MILLIS_PER_YEAR * equineHorse.getAge()));
-
 
             nbt.setString("EQUINE_HORSE", "true");
-            nbt.setString("EQUINE_OWNER_UUID", player.getUniqueId().toString());
-            nbt.setString("EQUINE_OWNER_NAME", player.getName());
+
             nbt.setString("EQUINE_DISCIPLINE", equineHorse.getDiscipline().name());
             nbt.setString("EQUINE_BREED", equineHorse.getBreed().name());
             nbt.setString("EQUINE_GENDER", equineHorse.getGender().name());
             nbt.setInteger("EQUINE_AGE", equineHorse.getAge());
-
             nbt.setDouble("EQUINE_HEIGHT", equineHorse.getHeight().getHands());
-
-            nbt.setDouble("EQUINE_BASE_SPEED", horse.getAttribute(Attribute.MOVEMENT_SPEED).getBaseValue());
-            nbt.setDouble("EQUINE_BASE_JUMP_POWER", horse.getJumpStrength());
-
             IntStream.range(0, equineHorse.getTraits().length)
                             .forEach(i -> nbt.setString("EQUINE_TRAIT_" + i, equineHorse.getTraits()[i].name()));
 
 
+            nbt.setLong("EQUINE_CLAIM_TIME", currentTime);
+            nbt.setLong("EQUINE_BIRTH_TIME", currentTime - (MILLIS_PER_YEAR * equineHorse.getAge()));
+            nbt.setString("EQUINE_OWNER_UUID", player.getUniqueId().toString());
+            nbt.setString("EQUINE_OWNER_NAME", player.getName());
+            nbt.setDouble("EQUINE_BASE_SPEED", horse.getAttribute(Attribute.MOVEMENT_SPEED).getBaseValue());
+            nbt.setDouble("EQUINE_BASE_JUMP_POWER", horse.getJumpStrength());
+            nbt.setString("EQUINE_SKULL_ID", randomSkullId());
+
             nbt.setString("EQUINE_IS_CROSS_TIED", "false");
 
         });
+    }
+
+    private String randomSkullId() {
+        List<String> idList = Arrays.asList("49654", "7280", "49652", "49651", "1154", "3920", "3919", "2912", "7649");
+        Random random = new Random();
+        int randomID = random.nextInt(idList.size());
+        String headID = idList.get(randomID);
+        return headID;
     }
 }
