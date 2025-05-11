4ab7eb2e7a01db06f72a445ff24e86507e254e1c
Endcrypt <pternej32@gmail.com>
2025-05-12 02:41:50 +0800

    update: database now only used when necessary

--- src/main/java/endcrypt/equinox/player/data/PlayerDataManager.java
+++ src/main/java/endcrypt/equinox/player/data/PlayerDataManager.java
@@ -2,12 +2,15 @@ package endcrypt.equinox.player.data;
 
 import endcrypt.equinox.EquinoxEquestrian;
 import org.bukkit.Bukkit;
+import org.bukkit.entity.AbstractHorse;
 import org.bukkit.entity.Player;
 import org.bukkit.event.Listener;
 
 import java.sql.SQLException;
 import java.util.HashMap;
+import java.util.List;
 import java.util.Map;
+import java.util.UUID;
 
 public class PlayerDataManager implements Listener {
 
@@ -23,20 +26,49 @@ public class PlayerDataManager implements Listener {
         return playerDataMap.get(player);
     }
 
-    public Map<Player, PlayerData> getPlayerDataMap() {
-        return playerDataMap;
+    public void load(Player player) {
+        this.loadPlayer(player);
     }
 
-    public void addPlayer(Player player) {
+    public void save(Player player) {
+        this.saveTokens(player);
+        this.savePlayerHorses(player);
+        this.playerDataMap.remove(player);
+    }
+
+    public void loadPlayer(Player player) {
         Bukkit.getScheduler().runTaskAsynchronously(plugin, () -> {
             try {
-                // Add the player to the database
-                // We don't need to check if player exists because we already do it in addPlayer method.
+                List<UUID> playerHorses = plugin.getDatabaseManager().getPlayerHorses(player);
                 plugin.getDatabaseManager().addPlayer(player);
-                playerDataMap.put(player, new PlayerData(null, plugin.getDatabaseManager().getTokenAmount(player)));
+                playerDataMap.put(player, new PlayerData(null, plugin.getDatabaseManager().getTokenAmount(player), playerHorses));
             } catch (SQLException e) {
                 e.printStackTrace();
             }
         });
+    }
+
+    public void savePlayerHorses(Player player) {
+        PlayerData playerData = playerDataMap.get(player);
+        List<UUID> playerHorses = playerData.getOwnedHorses();
+        try {
+            List<UUID> databaseHorses = plugin.getDatabaseManager().getPlayerHorses(player);
+            List<UUID> newHorses = playerHorses.stream().filter(horse -> !databaseHorses.contains(horse)).toList();
+
+            for (UUID uuid : newHorses) {
+                plugin.getDatabaseManager().addHorse((AbstractHorse) Bukkit.getEntity(uuid));
+            }
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
+    }
+
+    public void saveTokens(Player player) {
+        PlayerData playerData = playerDataMap.get(player);
+        try {
+            plugin.getDatabaseManager().setTokenAmount(player, playerData.getTokens());
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
     }
 }
