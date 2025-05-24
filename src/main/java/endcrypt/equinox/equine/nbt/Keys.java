c18b2ab65af1c25b391d14bb4f2d4a61c4afee2d
Endcrypt <pternej32@gmail.com>
2025-05-25 03:06:17 +0800

    update: added a method to directly read nbt from a horse using keys

--- src/main/java/endcrypt/equinox/equine/nbt/Keys.java
+++ src/main/java/endcrypt/equinox/equine/nbt/Keys.java
@@ -1,5 +1,10 @@
 package endcrypt.equinox.equine.nbt;
 
+import de.tr7zw.changeme.nbtapi.NBT;
+import lombok.Getter;
+import org.bukkit.entity.AbstractHorse;
+
+@Getter
 public enum Keys {
 
     CLAIM_TIME("EQUINE_CLAIM_TIME", System.currentTimeMillis()),
@@ -17,7 +22,9 @@ public enum Keys {
     OWNER_NAME("EQUINE_OWNER_NAME", ""),
     BASE_SPEED("EQUINE_BASE_SPEED", 0.0),
     BASE_JUMP("EQUINE_BASE_JUMP_POWER", 0.0),
-    IS_CROSS_TIED("EQUINE_IS_CROSS_TIED", "false");
+    IS_CROSS_TIED("EQUINE_IS_CROSS_TIED", "false"),
+    IS_GROOM_ITEM("EQUINE_GROOM_ITEM", "false"),
+    IS_LUNGING("EQUINE_IS_LUNGING", "false");
 
     public final String key;
     public final Object defaultValue;
@@ -27,11 +34,25 @@ public enum Keys {
         this.defaultValue = defaultValue;
     }
 
-    public String getKey() {
-        return key;
+    @SuppressWarnings("unchecked")
+    public static <T> T readPersistentData(AbstractHorse horse, Keys nbtKey) {
+        return NBT.getPersistentData(horse, nbt -> {
+            Object value;
+            if (nbtKey.getDefaultValue() instanceof String) {
+                value = nbt.getString(nbtKey.getKey());
+            } else if (nbtKey.getDefaultValue() instanceof Integer) {
+                value = nbt.getInteger(nbtKey.getKey());
+            } else if (nbtKey.getDefaultValue() instanceof Double) {
+                value = nbt.getDouble(nbtKey.getKey());
+            } else if (nbtKey.getDefaultValue() instanceof Long) {
+                value = nbt.getLong(nbtKey.getKey());
+            } else if (nbtKey.getDefaultValue() instanceof Boolean) {
+                value = nbt.getBoolean(nbtKey.getKey());
+            } else {
+                value = null;
+            }
+            return (T) value;
+        });
     }
 
-    public Object getDefaultValue() {
-        return defaultValue;
-    }
 }
