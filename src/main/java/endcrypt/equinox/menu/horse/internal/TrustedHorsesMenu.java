84250167625903d7867a6607b985e15daabc965b
Endcrypt <pternej32@gmail.com>
2025-05-28 12:59:47 +0800

    add: added TrustedHorsesMenu

---
+++ src/main/java/endcrypt/equinox/menu/horse/internal/TrustedHorsesMenu.java
@@ -0,0 +1,16 @@
+package endcrypt.equinox.menu.horse.internal;
+
+import endcrypt.equinox.EquinoxEquestrian;
+import org.bukkit.entity.Player;
+
+public class TrustedHorsesMenu {
+
+    private final EquinoxEquestrian plugin;
+    public TrustedHorsesMenu(EquinoxEquestrian plugin) {
+        this.plugin = plugin;
+    }
+
+    public void open(Player player) {
+
+    }
+}
