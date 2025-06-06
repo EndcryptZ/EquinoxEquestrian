8d562dd57b22095ad6ce2ddf828adb4f133adabe
Endcrypt <pternej32@gmail.com>
2025-06-06 21:07:18 +0800

    update: database fixed

--- src/main/java/endcrypt/equinox/database/DatabaseManager.java
+++ src/main/java/endcrypt/equinox/database/DatabaseManager.java
@@ -97,74 +97,72 @@ public class DatabaseManager implements Listener {
     }
 
     public void addHorse(AbstractHorse horse) {
-        Bukkit.getScheduler().runTaskAsynchronously(plugin, () -> {
-            boolean isSuccess = NBT.getPersistentData(horse, nbt -> {
-                String uuid = horse.getUniqueId().toString();
-                String ownerUuid = nbt.getString(Keys.OWNER_UUID.getKey());
-                String displayName = horse.getCustomName();
-                String discipline = nbt.getString(Keys.DISCIPLINE.getKey());
-                String breed1 = nbt.getString(Keys.BREED_PREFIX.getKey() + "0");
-                String breed2 = nbt.getString(Keys.BREED_PREFIX.getKey() + "1");
-                String prominentBreed = nbt.getString(Keys.PROMINENT_BREED.getKey());
-                String coatColor = nbt.getString(Keys.COAT_COLOR.getKey());
-                String coatModifier = nbt.getString(Keys.COAT_MODIFIER.getKey());
-                String gender = nbt.getString(Keys.GENDER.getKey());
-                int age = nbt.getInteger(Keys.AGE.getKey());
-                double height = nbt.getDouble(Keys.HEIGHT.getKey());
-                String trait1 = nbt.getString(Keys.TRAIT_PREFIX.getKey() + "0");
-                String trait2 = nbt.getString(Keys.TRAIT_PREFIX.getKey() + "1");
-                String trait3 = nbt.getString(Keys.TRAIT_PREFIX.getKey() + "2");
-                long claimTime = nbt.getLong(Keys.CLAIM_TIME.getKey());
-                long birthTime = nbt.getLong(Keys.BIRTH_TIME.getKey());
-                String ownerName = nbt.getString(Keys.OWNER_NAME.getKey());
-                double baseSpeed = nbt.getDouble(Keys.BASE_SPEED.getKey());
-                double baseJump = nbt.getDouble(Keys.BASE_JUMP.getKey());
-                String skullId = nbt.getString(Keys.SKULL_ID.getKey());
-                String lastWorld = nbt.getString(Keys.LAST_WORLD.getKey());
-                double lastX = nbt.getDouble(Keys.LAST_LOCATION_X.getKey());
-                double lastY = nbt.getDouble(Keys.LAST_LOCATION_Y.getKey());
-                double lastZ = nbt.getDouble(Keys.LAST_LOCATION_Z.getKey());
+        boolean isSuccess = NBT.getPersistentData(horse, nbt -> {
+            String uuid = horse.getUniqueId().toString();
+            String ownerUuid = nbt.getString(Keys.OWNER_UUID.getKey());
+            String displayName = horse.getCustomName();
+            String discipline = nbt.getString(Keys.DISCIPLINE.getKey());
+            String breed1 = nbt.getString(Keys.BREED_PREFIX.getKey() + "0");
+            String breed2 = nbt.getString(Keys.BREED_PREFIX.getKey() + "1");
+            String prominentBreed = nbt.getString(Keys.PROMINENT_BREED.getKey());
+            String coatColor = nbt.getString(Keys.COAT_COLOR.getKey());
+            String coatModifier = nbt.getString(Keys.COAT_MODIFIER.getKey());
+            String gender = nbt.getString(Keys.GENDER.getKey());
+            int age = nbt.getInteger(Keys.AGE.getKey());
+            double height = nbt.getDouble(Keys.HEIGHT.getKey());
+            String trait1 = nbt.getString(Keys.TRAIT_PREFIX.getKey() + "0");
+            String trait2 = nbt.getString(Keys.TRAIT_PREFIX.getKey() + "1");
+            String trait3 = nbt.getString(Keys.TRAIT_PREFIX.getKey() + "2");
+            long claimTime = nbt.getLong(Keys.CLAIM_TIME.getKey());
+            long birthTime = nbt.getLong(Keys.BIRTH_TIME.getKey());
+            String ownerName = nbt.getString(Keys.OWNER_NAME.getKey());
+            double baseSpeed = nbt.getDouble(Keys.BASE_SPEED.getKey());
+            double baseJump = nbt.getDouble(Keys.BASE_JUMP.getKey());
+            String skullId = nbt.getString(Keys.SKULL_ID.getKey());
+            String lastWorld = nbt.getString(Keys.LAST_WORLD.getKey());
+            double lastX = nbt.getDouble(Keys.LAST_LOCATION_X.getKey());
+            double lastY = nbt.getDouble(Keys.LAST_LOCATION_Y.getKey());
+            double lastZ = nbt.getDouble(Keys.LAST_LOCATION_Z.getKey());
 
-                try (PreparedStatement ps = connection.prepareStatement(
-                        "INSERT INTO EQUINE_HORSES (" +
-                                "uuid, owner_uuid, display_name, discipline, breed_1, breed_2, prominent_breed, coat_color, coat_modifier, " +
-                                "gender, age, height, trait_1, trait_2, trait_3, claim_time, birth_time, owner_name, base_speed, base_jump_power, skull_id, last_world, last_location_x, last_location_y, last_location_z ) " +
-                                "VALUES (?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?)")) {
+            try (PreparedStatement ps = connection.prepareStatement(
+                    "INSERT INTO EQUINE_HORSES (" +
+                            "uuid, owner_uuid, display_name, discipline, breed_1, breed_2, prominent_breed, coat_color, coat_modifier, " +
+                            "gender, age, height, trait_1, trait_2, trait_3, claim_time, birth_time, owner_name, base_speed, base_jump_power, skull_id, last_world, last_location_x, last_location_y, last_location_z ) " +
+                            "VALUES (?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?)")) {
 
-                    ps.setString(1, uuid);
-                    ps.setString(2, ownerUuid);
-                    ps.setString(3, displayName);
-                    ps.setString(4, discipline);
-                    ps.setString(5, breed1);
-                    ps.setString(6, breed2);
-                    ps.setString(7, prominentBreed);
-                    ps.setString(8, coatColor);        // added
-                    ps.setString(9, coatModifier);     // added
-                    ps.setString(10, gender);
-                    ps.setInt(11, age);
-                    ps.setDouble(12, height);
-                    ps.setString(13, trait1);
-                    ps.setString(14, trait2);
-                    ps.setString(15, trait3);
-                    ps.setLong(16, claimTime);
-                    ps.setLong(17, birthTime);
-                    ps.setString(18, ownerName);
-                    ps.setDouble(19, baseSpeed);
-                    ps.setDouble(20, baseJump);
-                    ps.setString(21, skullId);
-                    ps.setString(22, lastWorld);
-                    ps.setDouble(23, lastX);
-                    ps.setDouble(24, lastY);
-                    ps.setDouble(25, lastZ);
+                ps.setString(1, uuid);
+                ps.setString(2, ownerUuid);
+                ps.setString(3, displayName);
+                ps.setString(4, discipline);
+                ps.setString(5, breed1);
+                ps.setString(6, breed2);
+                ps.setString(7, prominentBreed);
+                ps.setString(8, coatColor);        // added
+                ps.setString(9, coatModifier);     // added
+                ps.setString(10, gender);
+                ps.setInt(11, age);
+                ps.setDouble(12, height);
+                ps.setString(13, trait1);
+                ps.setString(14, trait2);
+                ps.setString(15, trait3);
+                ps.setLong(16, claimTime);
+                ps.setLong(17, birthTime);
+                ps.setString(18, ownerName);
+                ps.setDouble(19, baseSpeed);
+                ps.setDouble(20, baseJump);
+                ps.setString(21, skullId);
+                ps.setString(22, lastWorld);
+                ps.setDouble(23, lastX);
+                ps.setDouble(24, lastY);
+                ps.setDouble(25, lastZ);
 
-                    ps.executeUpdate();
-                    return true;
-                } catch (SQLException e) {
-                    System.err.println("Failed to insert horse into database: " + uuid);
-                    e.printStackTrace();
-                    return false;
-                }
-            });
+                ps.executeUpdate();
+                return true;
+            } catch (SQLException e) {
+                System.err.println("Failed to insert horse into database: " + uuid);
+                e.printStackTrace();
+                return false;
+            }
         });
     }
 
@@ -184,7 +182,7 @@ public class DatabaseManager implements Listener {
                 while (resultSet.next()) {
                     EquineLiveHorse horse = new EquineLiveHorse();
                     horse.setUuid(UUID.fromString(resultSet.getString("uuid")));
-                    horse.setOwnerUUID(UUID.fromString(resultSet.getString("owner_uuid")));
+                    horse.setOwnerUUID(resultSet.getString("owner_uuid"));
                     horse.setName(resultSet.getString("display_name"));
                     horse.setDiscipline(Discipline.getDisciplineByName(resultSet.getString("discipline")));
                     List<Breed> breeds = new ArrayList<>();
@@ -273,79 +271,77 @@ public class DatabaseManager implements Listener {
     }
 
     public void updateHorse(AbstractHorse horse) {
-        Bukkit.getScheduler().runTaskAsynchronously(plugin, () -> {
+        boolean isSuccess = NBT.getPersistentData(horse, nbt -> {
+            String uuid = horse.getUniqueId().toString();
+            String ownerUuid = nbt.getString(Keys.OWNER_UUID.getKey());
+            String displayName = horse.getCustomName();
+            String discipline = nbt.getString(Keys.DISCIPLINE.getKey());
+            String breed1 = nbt.getString(Keys.BREED_PREFIX.getKey() + "0");
+            String breed2 = nbt.getString(Keys.BREED_PREFIX.getKey() + "1");
+            String prominentBreed = nbt.getString(Keys.PROMINENT_BREED.getKey());
+            String coatColor = nbt.getString(Keys.COAT_COLOR.getKey());
+            String coatModifier = nbt.getString(Keys.COAT_MODIFIER.getKey());
+            String gender = nbt.getString(Keys.GENDER.getKey());
+            int age = nbt.getInteger(Keys.AGE.getKey());
+            double height = nbt.getDouble(Keys.HEIGHT.getKey());
+            String trait1 = nbt.getString(Keys.TRAIT_PREFIX.getKey() + "0");
+            String trait2 = nbt.getString(Keys.TRAIT_PREFIX.getKey() + "1");
+            String trait3 = nbt.getString(Keys.TRAIT_PREFIX.getKey() + "2");
+            long claimTime = nbt.getLong(Keys.CLAIM_TIME.getKey());
+            long birthTime = nbt.getLong(Keys.BIRTH_TIME.getKey());
+            String ownerName = nbt.getString(Keys.OWNER_NAME.getKey());
+            double baseSpeed = nbt.getDouble(Keys.BASE_SPEED.getKey());
+            double baseJump = nbt.getDouble(Keys.BASE_JUMP.getKey());
+            String skullId = nbt.getString(Keys.SKULL_ID.getKey());
+            String lastWorld = nbt.getString(Keys.LAST_WORLD.getKey());
+            double lastX = nbt.getDouble(Keys.LAST_LOCATION_X.getKey());
+            double lastY = nbt.getDouble(Keys.LAST_LOCATION_Y.getKey());
+            double lastZ = nbt.getDouble(Keys.LAST_LOCATION_Z.getKey());
 
-            boolean isSuccess = NBT.getPersistentData(horse, nbt -> {
-                String uuid = horse.getUniqueId().toString();
-                String ownerUuid = nbt.getString(Keys.OWNER_UUID.getKey());
-                String displayName = horse.getCustomName();
-                String discipline = nbt.getString(Keys.DISCIPLINE.getKey());
-                String breed1 = nbt.getString(Keys.BREED_PREFIX.getKey() + "0");
-                String breed2 = nbt.getString(Keys.BREED_PREFIX.getKey() + "1");
-                String prominentBreed = nbt.getString(Keys.PROMINENT_BREED.getKey());
-                String coatColor = nbt.getString(Keys.COAT_COLOR.getKey());
-                String coatModifier = nbt.getString(Keys.COAT_MODIFIER.getKey());
-                String gender = nbt.getString(Keys.GENDER.getKey());
-                int age = nbt.getInteger(Keys.AGE.getKey());
-                double height = nbt.getDouble(Keys.HEIGHT.getKey());
-                String trait1 = nbt.getString(Keys.TRAIT_PREFIX.getKey() + "0");
-                String trait2 = nbt.getString(Keys.TRAIT_PREFIX.getKey() + "1");
-                String trait3 = nbt.getString(Keys.TRAIT_PREFIX.getKey() + "2");
-                long claimTime = nbt.getLong(Keys.CLAIM_TIME.getKey());
-                long birthTime = nbt.getLong(Keys.BIRTH_TIME.getKey());
-                String ownerName = nbt.getString(Keys.OWNER_NAME.getKey());
-                double baseSpeed = nbt.getDouble(Keys.BASE_SPEED.getKey());
-                double baseJump = nbt.getDouble(Keys.BASE_JUMP.getKey());
-                String skullId = nbt.getString(Keys.SKULL_ID.getKey());
-                String lastWorld = nbt.getString(Keys.LAST_WORLD.getKey());
-                double lastX = nbt.getDouble(Keys.LAST_LOCATION_X.getKey());
-                double lastY = nbt.getDouble(Keys.LAST_LOCATION_Y.getKey());
-                double lastZ = nbt.getDouble(Keys.LAST_LOCATION_Z.getKey());
+            try (PreparedStatement ps = connection.prepareStatement(
+                    "UPDATE EQUINE_HORSES SET " +
+                            "owner_uuid = ?, display_name = ?, discipline = ?, breed_1 = ?, breed_2 = ?, prominent_breed = ?, " +
+                            "coat_color = ?, coat_modifier = ?, gender = ?, age = ?, height = ?, trait_1 = ?, trait_2 = ?, " +
+                            "trait_3 = ?, claim_time = ?, birth_time = ?, owner_name = ?, base_speed = ?, base_jump_power = ?, " +
+                            "skull_id = ?, last_world = ?, last_location_x = ?, last_location_y = ?, last_location_z = ? " +
+                            "WHERE uuid = ?")) {
 
-                try (PreparedStatement ps = connection.prepareStatement(
-                        "UPDATE EQUINE_HORSES SET " +
-                                "WHERE uuid = ?, owner_uuid = ?, display_name = ?, discipline = ?, breed_1 = ?, breed_2 = ?, prominent_breed = ?, " +
-                                "coat_color = ?, coat_modifier = ?, gender = ?, age = ?, height = ?, trait_1 = ?, trait_2 = ?, " +
-                                "trait_3 = ?, claim_time = ?, birth_time = ?, owner_name = ?, base_speed = ?, base_jump_power = ?, " +
-                                "skull_id = ?, last_world = ?, last_location_x = ?, last_location_y = ?, last_location_z = ?")) {
+                ps.setString(1, ownerUuid);
+                ps.setString(2, displayName);
+                ps.setString(3, discipline);
+                ps.setString(4, breed1);
+                ps.setString(5, breed2);
+                ps.setString(6, prominentBreed);
+                ps.setString(7, coatColor);
+                ps.setString(8, coatModifier);
+                ps.setString(9, gender);
+                ps.setInt(10, age);
+                ps.setDouble(11, height);
+                ps.setString(12, trait1);
+                ps.setString(13, trait2);
+                ps.setString(14, trait3);
+                ps.setLong(15, claimTime);
+                ps.setLong(16, birthTime);
+                ps.setString(17, ownerName);
+                ps.setDouble(18, baseSpeed);
+                ps.setDouble(19, baseJump);
+                ps.setString(20, skullId);
+                ps.setString(21, lastWorld);
+                ps.setDouble(22, lastX);
+                ps.setDouble(23, lastY);
+                ps.setDouble(24, lastZ);
+                ps.setString(25, uuid); // WHERE uuid = ?
 
-                    ps.setString(1, uuid);
-                    ps.setString(2, ownerUuid);
-                    ps.setString(3, displayName);
-                    ps.setString(4, discipline);
-                    ps.setString(5, breed1);
-                    ps.setString(6, breed2);
-                    ps.setString(7, prominentBreed);
-                    ps.setString(8, coatColor);
-                    ps.setString(9, coatModifier);
-                    ps.setString(10, gender);
-                    ps.setInt(11, age);
-                    ps.setDouble(12, height);
-                    ps.setString(13, trait1);
-                    ps.setString(14, trait2);
-                    ps.setString(15, trait3);
-                    ps.setLong(16, claimTime);
-                    ps.setLong(17, birthTime);
-                    ps.setString(18, ownerName);
-                    ps.setDouble(19, baseSpeed);
-                    ps.setDouble(20, baseJump);
-                    ps.setString(21, skullId);
-                    ps.setString(22, lastWorld);
-                    ps.setDouble(23, lastX);
-                    ps.setDouble(24, lastY);
-                    ps.setDouble(25, lastZ);
-
-                    ps.executeUpdate();
-                    return true;
-                } catch (SQLException e) {
-                    System.err.println("Failed to update horse in database: " + uuid);
-                    e.printStackTrace();
-                    return false;
-                }
-            });
+                ps.executeUpdate();
+                return true;
+            } catch (SQLException e) {
+                System.err.println("Failed to update horse in database: " + uuid);
+                e.printStackTrace();
+                return false;
+            }
         });
-
     }
+
 
 
 }
