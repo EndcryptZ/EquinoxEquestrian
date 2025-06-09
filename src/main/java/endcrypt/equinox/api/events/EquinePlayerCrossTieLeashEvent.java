d9339d9203bcb62d07d11c40bf90e4464f45d62f
Endcrypt <pternej32@gmail.com>
2025-06-09 23:04:18 +0800

    update: renamed

--- src/main/java/endcrypt/equinox/api/events/EquinePlayerCrossTieLeashEvent.java
+++ src/main/java/endcrypt/equinox/api/events/EquinePlayerCrossTieLeashEvent.java
@@ -1,29 +1,25 @@
 package endcrypt.equinox.api.events;
 
+import lombok.Getter;
 import org.bukkit.entity.AbstractHorse;
 import org.bukkit.event.Event;
 import org.bukkit.event.HandlerList;
+import org.jetbrains.annotations.NotNull;
 
-public class EquineCrossTieLeashEvent extends Event {
+@Getter
+public class EquinePlayerCrossTieLeashEvent extends Event {
 
     private static final HandlerList HANDLER_LIST = new HandlerList();
-    private AbstractHorse horse;
+    private final AbstractHorse horse;
 
     // Constructor: Include the horse and the side of the leash (left or right)
-    public EquineCrossTieLeashEvent(AbstractHorse horse) {
+    public EquinePlayerCrossTieLeashEvent(AbstractHorse horse) {
         this.horse = horse;
     }
 
-    public AbstractHorse getHorse() {
-        return horse;
-    }
-
     @Override
-    public HandlerList getHandlers() {
+    public @NotNull HandlerList getHandlers() {
         return HANDLER_LIST;
     }
 
-    public static HandlerList getHandlerList() {
-        return HANDLER_LIST;
-    }
 }
