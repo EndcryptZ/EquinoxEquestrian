41a656860f6f2f2d7ad29e045d65db8e6eab5b50
Endcrypt <pternej32@gmail.com>
2025-06-20 02:31:13 +0800

    update: added TimeUtils.java

--- src/main/java/endcrypt/equinox/utils/TimeUtils.java
+++ src/main/java/endcrypt/equinox/utils/TimeUtils.java
@@ -5,4 +5,12 @@ public class TimeUtils {
     public static long daysToMillis(int days) {
         return (long) days * 24 * 60 * 60 * 1000;
     }
+
+    public static long secondsToMillis(int seconds) {
+        return (long) seconds * 1000;
+    }
+
+    public static long minutesToMillis(int minutes) {
+        return (long) minutes * 60 * 1000;
+    }
 }
