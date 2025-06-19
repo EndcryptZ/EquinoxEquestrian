8cf1a023a3c6cd56493d774f64bc8af3f57562bb
Endcrypt <pternej32@gmail.com>
2025-06-20 02:33:29 +0800

    update: added permission to fix unwanted mounting if horse is cross-tied

--- src/main/java/endcrypt/equinox/equine/crosstie/EquineCrossTieListener.java
+++ src/main/java/endcrypt/equinox/equine/crosstie/EquineCrossTieListener.java
@@ -4,6 +4,7 @@ import de.tr7zw.changeme.nbtapi.NBT;
 import endcrypt.equinox.EquinoxEquestrian;
 import endcrypt.equinox.api.events.EquinePlayerCrossTieLeashEvent;
 import endcrypt.equinox.api.events.EquinePlayerCrossTieLeashRemovedEvent;
+import endcrypt.equinox.equine.EquineLiveHorse;
 import endcrypt.equinox.equine.EquineUtils;
 import endcrypt.equinox.equine.nbt.Keys;
 import endcrypt.equinox.utils.ColorUtils;
@@ -55,7 +56,14 @@ public class EquineCrossTieListener implements Listener {
             return;
         }
 
+        if (!EquineUtils.hasPermissionToHorse(player, horse)) {
+            player.sendMessage(ColorUtils.color(plugin.getPrefix() + "<red>You can't interact with this horse!"));
+            event.setCancelled(true);
+            return;
+        }
+
         if (EquineUtils.isCrossTied(horse)) {
+            event.setCancelled(true);
             return;
         }
 
