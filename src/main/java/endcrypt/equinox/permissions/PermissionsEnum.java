af01f4d9cee9e6ac89243ed949ce89d7aff5e7fd
Endcrypt <pternej32@gmail.com>
2025-07-29 03:59:07 +0800

    update: fix

--- src/main/java/endcrypt/equinox/permissions/PermissionsEnum.java
+++ src/main/java/endcrypt/equinox/permissions/PermissionsEnum.java
@@ -5,7 +5,7 @@ import lombok.Getter;
 @Getter
 public enum PermissionsEnum {
 
-    PERMISSION_HORSE_LIMIT("equinox.horse.limit"),
+    PERMISSION_HORSE_LIMIT("equinox.horse.limit");
 
 
     private final String permission;
