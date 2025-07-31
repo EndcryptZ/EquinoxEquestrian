618c935e2d19a2c31c8201e5cc89b3fdbe17fe06
Endcrypt <pternej32@gmail.com>
2025-07-31 20:01:29 +0800

    update: finish manure menu system

--- src/main/java/endcrypt/equinox/menu/shop/manure/ShopManureMenu.java
+++ src/main/java/endcrypt/equinox/menu/shop/manure/ShopManureMenu.java
@@ -4,7 +4,10 @@ import com.samjakob.spigui.buttons.SGButton;
 import com.samjakob.spigui.item.ItemBuilder;
 import com.samjakob.spigui.menu.SGMenu;
 import endcrypt.equinox.EquinoxEquestrian;
+import endcrypt.equinox.utils.ColorUtils;
+import net.kyori.adventure.text.minimessage.tag.resolver.Placeholder;
 import org.bukkit.Material;
+import org.bukkit.Sound;
 import org.bukkit.entity.Player;
 import org.bukkit.event.inventory.InventoryClickEvent;
 import org.bukkit.inventory.Inventory;
@@ -29,6 +32,16 @@ public class ShopManureMenu {
         int playerManureAmount = plugin.getPlayerDataManager().getPlayerData(player).getManure();
         SGMenu gui = plugin.getSpiGUI().create("Manure Market (" + playerManureAmount + " Manure)", 2, "Manure Market");
 
+        gui.setButton(0, sellButton(player, 5, playerManureAmount));
+        gui.setButton(1, sellButton(player, 10, playerManureAmount));
+        gui.setButton(2, sellButton(player, 25, playerManureAmount));
+
+        gui.setButton(8, sellAllButton(player, playerManureAmount));
+
+        gui.setButton(9, backButton(player));
+
+
+
         return gui.getInventory();
     }
 
@@ -45,13 +58,63 @@ public class ShopManureMenu {
         }
 
         return new SGButton(
-                new ItemBuilder(Material.RED_TERRACOTTA )
+                new ItemBuilder(Material.RED_TERRACOTTA)
                         .name("&fSell " + amount)
                         .lore(lore)
                         .build()
         )
-                .withListener((InventoryClickEvent event ) -> {
-
+                .withListener((InventoryClickEvent event) -> {
+                    sellPlayerManure(player, amount);
                 });
+    }
+
+    private SGButton sellAllButton(Player player, int playerManureAmount) {
+        return new SGButton(
+                new ItemBuilder(Material.RED_TERRACOTTA)
+                        .name("&fSell " + playerManureAmount)
+                        .lore(
+                                "&aClick to sell " + playerManureAmount,
+                                "&7manure to sell."
+                        )
+                        .build()
+        )
+                .withListener((InventoryClickEvent event) -> {
+                    sellPlayerManure(player, playerManureAmount);
+                });
+    }
+
+    private SGButton backButton(Player player) {
+        return new SGButton(
+                new ItemBuilder(Material.MAP)
+                        .name("&7[<] Previous Page")
+                        .build()
+        )
+                .withListener((InventoryClickEvent event ) -> {
+                    player.performCommand("shop");
+                });
+    }
+
+    private void sellPlayerManure(Player player, int amount) {
+        int playerManureAmount = plugin.getPlayerDataManager().getPlayerData(player).getManure();
+        boolean hasEnough = playerManureAmount >= amount;
+        if(!hasEnough) {
+            player.sendMessage(ColorUtils.color("<prefix><red>You don't have enough manure to sell!",
+                    Placeholder.parsed("prefix", plugin.getPrefix())));
+            player.playSound(player.getLocation(), Sound.BLOCK_ANVIL_LAND, 1f, 1f);
+            return;
+        }
+
+        int manureResult = playerManureAmount - amount;
+        double gain = amount * 0.5;
+        plugin.getPlayerDataManager().getPlayerData(player).setManure(manureResult);
+        plugin.getEcon().depositPlayer(player, gain);
+        player.sendMessage(ColorUtils.color(
+                "<prefix><green>you've sold <yellow><amount> <green>manure for <gold>$<gain><green>!",
+                Placeholder.parsed("prefix", plugin.getPrefix()),
+                Placeholder.parsed("amount", String.valueOf(amount)),
+                Placeholder.parsed("gain", String.valueOf(gain))
+        ));
+        player.playSound(player.getLocation(), Sound.ENTITY_EXPERIENCE_ORB_PICKUP, 1f, 1f);
+        open(player);
     }
 }
