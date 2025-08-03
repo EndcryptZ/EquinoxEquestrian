a2542636b5fa1c97d25064433c06349867a9d947
Endcrypt <pternej32@gmail.com>
2025-08-03 21:09:54 +0800

    rename ShopManureMenu -> ShopManureSellMenu

--- src/main/java/endcrypt/equinox/menu/shop/ShopMenuManager.java
+++ src/main/java/endcrypt/equinox/menu/shop/ShopMenuManager.java
@@ -1,16 +1,16 @@
 package endcrypt.equinox.menu.shop;
 
 import endcrypt.equinox.EquinoxEquestrian;
-import endcrypt.equinox.menu.shop.manure.ShopManureMenu;
+import endcrypt.equinox.menu.shop.manure.ShopManureSellMenu;
 import lombok.Getter;
 
 @Getter
 public class ShopMenuManager {
 
     private final EquinoxEquestrian plugin;
-    private final ShopManureMenu shopManureMenu;
+    private final ShopManureSellMenu shopManureMenu;
     public ShopMenuManager(EquinoxEquestrian plugin) {
         this.plugin = plugin;
-        shopManureMenu = new ShopManureMenu(plugin);
+        shopManureMenu = new ShopManureSellMenu(plugin);
     }
 }
