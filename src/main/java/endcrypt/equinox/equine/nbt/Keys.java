764ef677b5d647eef0a7158e9a58a3ccbeed04bc
Endcrypt <pternej32@gmail.com>
2025-08-10 03:46:52 +0800

    add class types

--- src/main/java/endcrypt/equinox/equine/nbt/Keys.java
+++ src/main/java/endcrypt/equinox/equine/nbt/Keys.java
@@ -7,66 +7,68 @@ import org.bukkit.entity.AbstractHorse;
 @Getter
 public enum Keys {
 
-    CLAIM_TIME("EQUINE_CLAIM_TIME", System.currentTimeMillis()),
-    SKULL_ID("EQUINE_SKULL_ID", "49654"),
-    IS_EQUINE("EQUINE_HORSE", "true"),
-    DISCIPLINE("EQUINE_DISCIPLINE", ""),
-    BREED_PREFIX("EQUINE_BREED_", ""),
-    PROMINENT_BREED("EQUINE_PROMINENT_BREED", ""),
-    GENDER("EQUINE_GENDER", ""),
-    AGE("EQUINE_AGE", 0),
-    HEIGHT("EQUINE_HEIGHT", 0.0),
-    TRAIT_PREFIX("EQUINE_TRAIT_", ""),
-    BIRTH_TIME("EQUINE_BIRTH_TIME", System.currentTimeMillis()),
-    OWNER_UUID("EQUINE_OWNER_UUID", ""),
-    OWNER_NAME("EQUINE_OWNER_NAME", ""),    
-    BASE_SPEED("EQUINE_BASE_SPEED", 0.0),
-    BASE_JUMP("EQUINE_BASE_JUMP_POWER", 0.0),
-    IS_CROSS_TIED("EQUINE_IS_CROSS_TIED", "false"),
-    IS_GROOM_ITEM("EQUINE_GROOM_ITEM", "false"),
-    IS_LUNGING("EQUINE_IS_LUNGING", "false"),
-    COAT_COLOR("EQUINE_COAT_COLOR", ""),
-    COAT_MODIFIER("EQUINE_COAT_MODIFIER", ""),
-    LAST_WORLD("EQUINE_LAST_WORLD", ""),
-    LAST_LOCATION_X("EQUINE_LAST_LOCATION_X", 0.0),
-    LAST_LOCATION_Y("EQUINE_LAST_LOCATION_Y", 0.0),
-    LAST_LOCATION_Z("EQUINE_LAST_LOCATION_Z", 0.0),
-    IS_PUBLIC("EQUINE_IS_PUBLIC", "false"),
+    CLAIM_TIME("EQUINE_CLAIM_TIME", System.currentTimeMillis(), Long.class),
+    SKULL_ID("EQUINE_SKULL_ID", "49654", String.class),
+    IS_EQUINE("EQUINE_HORSE", "true", Boolean.class),
+    DISCIPLINE("EQUINE_DISCIPLINE", "", String.class),
+    BREED_PREFIX("EQUINE_BREED_", "", String.class),
+    PROMINENT_BREED("EQUINE_PROMINENT_BREED", "", String.class),
+    GENDER("EQUINE_GENDER", "", String.class),
+    AGE("EQUINE_AGE", 0, Integer.class),
+    HEIGHT("EQUINE_HEIGHT", 0.0, Double.class),
+    TRAIT_PREFIX("EQUINE_TRAIT_", "", String.class),
+    BIRTH_TIME("EQUINE_BIRTH_TIME", System.currentTimeMillis(), Long.class),
+    OWNER_UUID("EQUINE_OWNER_UUID", "", String.class),
+    OWNER_NAME("EQUINE_OWNER_NAME", "", String.class),
+    BASE_SPEED("EQUINE_BASE_SPEED", 0.0, Double.class),
+    BASE_JUMP("EQUINE_BASE_JUMP_POWER", 0.0, Double.class),
+    IS_CROSS_TIED("EQUINE_IS_CROSS_TIED", "false", Boolean.class),
+    IS_GROOM_ITEM("EQUINE_GROOM_ITEM", "false", Boolean.class),
+    IS_LUNGING("EQUINE_IS_LUNGING", "false", Boolean.class),
+    COAT_COLOR("EQUINE_COAT_COLOR", "", String.class),
+    COAT_MODIFIER("EQUINE_COAT_MODIFIER", "", String.class),
+    LAST_WORLD("EQUINE_LAST_WORLD", "", String.class),
+    LAST_LOCATION_X("EQUINE_LAST_LOCATION_X", 0.0, Double.class),
+    LAST_LOCATION_Y("EQUINE_LAST_LOCATION_Y", 0.0, Double.class),
+    LAST_LOCATION_Z("EQUINE_LAST_LOCATION_Z", 0.0, Double.class),
+    IS_PUBLIC("EQUINE_IS_PUBLIC", "false", Boolean.class),
 
     // Breeding & Pregnancy
-    IS_IN_HEAT("EQUINE_IS_IN_HEAT", "false"),
-    LAST_IN_HEAT("EQUINE_LAST_IN_HEAT", System.currentTimeMillis()),
+    IS_IN_HEAT("EQUINE_IS_IN_HEAT", "false", Boolean.class),
+    LAST_IN_HEAT("EQUINE_LAST_IN_HEAT", System.currentTimeMillis(), Long.class),
 
-    IS_PREGNANT("EQUINE_IS_PREGNANT", "false"),
-    PREGNANCY_START_TIME("EQUINE_PREGNANCY_START", System.currentTimeMillis()),
-    PREGNANCY_PARTNER("EQUINE_PREGNANCY_PARTNER", ""),
-    INSTANT_FOAL("EQUINE_INSTANT_FOAL", "false"),
+    IS_PREGNANT("EQUINE_IS_PREGNANT", "false", Boolean.class),
+    PREGNANCY_START_TIME("EQUINE_PREGNANCY_START", System.currentTimeMillis(), Long.class),
+    PREGNANCY_PARTNER("EQUINE_PREGNANCY_PARTNER", "", String.class),
+    INSTANT_FOAL("EQUINE_INSTANT_FOAL", "false", Boolean.class),
 
-    IS_BREEDING("EQUINE_IS_BREEDING", "false"),
-    BREEDING_START_TIME("EQUINE_BREEDING_START", System.currentTimeMillis()),
-    BREEDING_PARTNER("EQUINE_BREEDING_PARTNER", ""),
-    INSTANT_BREED("EQUINE_INSTANT_BREED", "false"),
+    IS_BREEDING("EQUINE_IS_BREEDING", "false", Boolean.class),
+    BREEDING_START_TIME("EQUINE_BREEDING_START", System.currentTimeMillis(), Long.class),
+    BREEDING_PARTNER("EQUINE_BREEDING_PARTNER", "", String.class),
+    INSTANT_BREED("EQUINE_INSTANT_BREED", "false", Boolean.class),
 
     // Waste
-    LAST_PEE("EQUINE_LAST_PEE", System.currentTimeMillis()),
-    LAST_POOP("EQUINE_LAST_POOP", System.currentTimeMillis()),
+    LAST_PEE("EQUINE_LAST_PEE", System.currentTimeMillis(), Long.class),
+    LAST_POOP("EQUINE_LAST_POOP", System.currentTimeMillis(), Long.class),
 
     // Hunger
-    HUNGER_PERCENTAGE("EQUINE_HUNGER_PERCENTAGE", 100.0),
-    LAST_HUNGER_UPDATE("EQUINE_LAST_HUNGER_UPDATE", System.currentTimeMillis()),
-    LAST_SEEK_FOOD("EQUINE_LAST_SEEK_FOOD", System.currentTimeMillis()),
+    HUNGER_PERCENTAGE("EQUINE_HUNGER_PERCENTAGE", 100.0, Double.class),
+    LAST_HUNGER_UPDATE("EQUINE_LAST_HUNGER_UPDATE", System.currentTimeMillis(), Long.class),
+    LAST_SEEK_FOOD("EQUINE_LAST_SEEK_FOOD", System.currentTimeMillis(), Long.class),
 
     // Thirst
-    THIRST_PERCENTAGE("EQUINE_THIRST_PERCENTAGE", 100.0),
-    LAST_THIRST_UPDATE("EQUINE_LAST_THIRST_UPDATE", System.currentTimeMillis());
+    THIRST_PERCENTAGE("EQUINE_THIRST_PERCENTAGE", 100.0, Double.class),
+    LAST_THIRST_UPDATE("EQUINE_LAST_THIRST_UPDATE", System.currentTimeMillis(), Long.class);
 
 
     public final String key;
     public final Object defaultValue;
+    public final Class<?> type;
 
-    Keys(String key, Object defaultValue) {
+    Keys(String key, Object defaultValue, Class<?> type) {
         this.key = key;
         this.defaultValue = defaultValue;
+        this.type = type;
     }
 
     @SuppressWarnings("unchecked")
