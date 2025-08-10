ec13e2fe8484139a779f6ae06805a27f74495ab1
Endcrypt <pternej32@gmail.com>
2025-08-11 06:14:52 +0800

    clean up

--- src/main/java/endcrypt/equinox/equine/hunger/EquineHunger.java
+++ src/main/java/endcrypt/equinox/equine/hunger/EquineHunger.java
@@ -27,8 +27,15 @@ public class EquineHunger {
                 7 // radius
         );
 
-        if (targetBlock == null) return;
-        if (Keys.readPersistentData(horse, Keys.IS_IN_WATER_TASK)) return;
+        if (targetBlock == null) {
+            Keys.writePersistentData(horse, Keys.IS_IN_FOOD_TASK, false);
+            return;
+        }
+        if (Keys.readPersistentData(horse, Keys.IS_IN_WATER_TASK)) {
+            Keys.writePersistentData(horse, Keys.IS_IN_FOOD_TASK, false);
+            return;
+        }
+
         Keys.writePersistentData(horse, Keys.IS_IN_FOOD_TASK, true);
 
 
@@ -36,6 +43,7 @@ public class EquineHunger {
         Bukkit.getScheduler().runTaskTimer(plugin, task -> {
             if (!horse.isValid() || horse.isDead()) {
                 task.cancel();
+                Keys.writePersistentData(horse, Keys.IS_IN_FOOD_TASK, false);
                 return;
             }
 
