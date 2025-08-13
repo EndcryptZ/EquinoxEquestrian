d9f41f44d7e8626a8d5d4bc9b7a51991bf4aadf0
Endcrypt <pternej32@gmail.com>
2025-08-14 04:36:47 +0800

    add on lunge custom event

---
+++ src/main/java/endcrypt/equinox/api/events/EquinePlayerLungeHorseEvent.java
@@ -0,0 +1,42 @@
+package endcrypt.equinox.api.events;
+
+import org.bukkit.entity.AbstractHorse;
+import org.bukkit.entity.Player;
+import org.bukkit.event.Cancellable;
+import org.bukkit.event.Event;
+import org.bukkit.event.HandlerList;
+import org.jetbrains.annotations.NotNull;
+
+public class EquinePlayerLungeHorseEvent extends Event implements Cancellable {
+
+    private static final HandlerList HANDLER_LIST = new HandlerList();
+    private final Player player;
+    private final AbstractHorse horse;
+
+    private boolean cancelled;
+
+    // Constructor: Include the horse and the side of the leash (left or right)
+    public EquinePlayerLungeHorseEvent(Player player, AbstractHorse horse) {
+        this.player = player;
+        this.horse = horse;
+    }
+
+    @Override
+    public @NotNull HandlerList getHandlers() {
+        return HANDLER_LIST;
+    }
+
+    public static HandlerList getHandlerList() {
+        return HANDLER_LIST;
+    }
+
+    @Override
+    public boolean isCancelled() {
+        return cancelled;
+    }
+
+    @Override
+    public void setCancelled(boolean cancel) {
+        this.cancelled = cancel;
+    }
+}
