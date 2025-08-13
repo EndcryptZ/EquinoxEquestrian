0311091c7d372bdfa76765c14aa5c7a82639d2c2
Endcrypt <pternej32@gmail.com>
2025-08-14 04:37:01 +0800

    add player to cross-tie leash event

--- src/main/java/endcrypt/equinox/api/events/EquinePlayerCrossTieLeashEvent.java
+++ src/main/java/endcrypt/equinox/api/events/EquinePlayerCrossTieLeashEvent.java
@@ -2,6 +2,7 @@ package endcrypt.equinox.api.events;
 
 import lombok.Getter;
 import org.bukkit.entity.AbstractHorse;
+import org.bukkit.entity.Player;
 import org.bukkit.event.Event;
 import org.bukkit.event.HandlerList;
 import org.jetbrains.annotations.NotNull;
@@ -10,10 +11,12 @@ import org.jetbrains.annotations.NotNull;
 public class EquinePlayerCrossTieLeashEvent extends Event {
 
     private static final HandlerList HANDLER_LIST = new HandlerList();
+    private final Player player;
     private final AbstractHorse horse;
 
     // Constructor: Include the horse and the side of the leash (left or right)
-    public EquinePlayerCrossTieLeashEvent(AbstractHorse horse) {
+    public EquinePlayerCrossTieLeashEvent(Player player, AbstractHorse horse) {
+        this.player = player;
         this.horse = horse;
     }
 
